85b8121f6501fd7ca0524df35b3bfb6d35e9c1e5
livehybrid <will@livehybrid.com>
2024-07-21 23:52:16 +0100

    Initial commit

---
+++ package/bin/unifi_collect_hosts_helper.py
@@ -0,0 +1,12 @@
+from unifi_input_helper import unifiInput
+from splunklib import modularinput as smi
+
+input_helper = unifiInput("hosts")
+
+
+def validate_input(definition: smi.ValidationDefinition):
+    return input_helper.validate_input(definition)
+
+
+def stream_events(inputs: smi.InputDefinition, event_writer: smi.EventWriter):
+    return input_helper.stream_events(inputs, event_writer)
